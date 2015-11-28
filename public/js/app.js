d388b14776b379f0ef8a48e664e2f57ab0962fbd
Donghwa Lee <donghwalee328@gmail.com>
2015-11-28 11:34:32 -0500

    correct and incorrect answers bring up the feedback

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,51 @@
 var app = angular.module('QuizApp', []);
 
-app.controller('CreateController', ['$http', function($http){
+app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce){
   var controller = this;
+
+  $scope.activeQuestion = 0;
+  $scope.randQuestionIndex = 0;
 
   $http.get('questions').success(function(data){
     controller.questions = data;
+    numQuestions = data.length;
+    randQuestionIndex = Math.floor(Math.random() * numQuestions);
     console.log(data);
+    console.log(randQuestionIndex);
+    console.log(data[randQuestionIndex]);
   });
+
+  $scope.selectAnswer = function(qIndex, aIndex) {
+    console.log("Q: " + qIndex);
+    console.log("A: " + aIndex);
+    var questionState = controller.questions[qIndex].questionState;
+    if ( questionState != 'answered' ) {
+      controller.questions[qIndex].selectedAnswer = aIndex;
+      var correctAnswer = controller.questions[qIndex].correct_answer_id;
+      controller.questions[qIndex].correctAnswer = correctAnswer;
+      console.log("Correct: " + correctAnswer );
+      if ( aIndex === correctAnswer ) {
+        controller.questions[qIndex].correctness = 'correct';
+        console.log("Correct");
+      } else {
+        controller.questions[qIndex].correctness = 'incorrect';
+        console.log("Incorrect");
+      }
+      controller.questions[qIndex].questionState = 'answered';
+    };
+
+  };
+
+  $scope.isSelected = function(qIndex, aIndex) {
+    return controller.questions[qIndex].selectedAnswer === aIndex;
+  };
+
+  $scope.isCorrect = function(qIndex, aIndex) {
+    return controller.questions[qIndex].correctAnswer === aIndex;
+  };
+
+
+
 
   this.create = function(){
     $http.post('/questions', {
